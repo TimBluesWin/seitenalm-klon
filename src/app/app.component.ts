aa7675b66bb66418e577ad8944927c2682e4dd42
Timothy Aditya Sutantyo <timisutantyo@gmail.com>
2024-07-11 17:42:25 +0200

    Art der Commit: Feature
    Die Validierungen für die vorhandenen Felder hinzufügen.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,15 @@ import { CommonModule } from "@angular/common";
 })
 export class AppComponent {
   anmeldungForm = new FormGroup({
+    // Ich bekomme dieses Regex für die Validierung der Namen von dieser Website:
+    // https://a-tokyo.medium.com/first-and-last-name-validation-for-forms-and-databases-d3edf29ad29d
     vorname: new FormControl("", [
       Validators.required,
+      Validators.pattern("^[a-zA-Z\xC0-\uFFFF]+([ \\-']{0,1}[a-zA-Z\xC0-\uFFFF]+){0,2}[.]{0,1}$")
     ]),
     nachname: new FormControl("", [
       Validators.required,
+      Validators.pattern("^[a-zA-Z\xC0-\uFFFF]+([ \\-']{0,1}[a-zA-Z\xC0-\uFFFF]+){0,2}[.]{0,1}$")
     ]),
     // Ich benutze keine FormGroup, weil es aussieht, dass das originale
     // Formular keine Formgroup verwendet (kein Headings usw.)
@@ -27,8 +31,11 @@ export class AppComponent {
       Validators.required,
     ]),
     stadt: new FormControl("", [Validators.required]),
-    email: new FormControl(""),
-    plz: new FormControl(""),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    // Eigentlich glaube ich besser, wenn die PLZ nicht 4 Zahlen muss.
+    // Ein Beispiel: In Deutschland hat die PLZ 5 Digits.
+    plz: new FormControl("", [Validators.required, 
+      Validators.pattern("^[0-9]{4}$"),]),
     telefon: new FormControl(""),
     fragen: new FormControl("")
   });
